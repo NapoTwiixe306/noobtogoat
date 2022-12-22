1af157ec675484e2b21c559134dce6eac0d7d5c8
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-12-22 13:35:27 +0100

    delete main

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -5,9 +5,3 @@ void ft_print_alphabet(void)
     write(1, "abcdefghijklmnopqrstuvwxyz", 26);
     write(1, "\n", 1);
 }
-
-int main()
-{
-    ft_print_alphabet();
-    return (0);
-}
--- C00/ex02/ft_print_reverse_alphabet.c
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -5,9 +5,3 @@ void ft_print_reverse_alphabet(void)
     write(1, "zyxwvutsrqpomnlkjihgfedcba", 26);
     write(1, "\n", 1);
 }
-
-int main()
-{
-    ft_print_reverse_alphabet();
-    return (0);
-}
