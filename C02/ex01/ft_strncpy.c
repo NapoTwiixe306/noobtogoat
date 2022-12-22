b614a3779e9aa3f78be60073922589689663ae88
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-12-22 15:05:18 +0100

    comment main

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: Julien Milants <julien.milants@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 14:51:39 by Julien Mila       #+#    #+#             */
-/*   Updated: 2022/12/22 15:04:18 by Julien Mila      ###   ########.fr       */
+/*   Updated: 2022/12/22 15:05:13 by Julien Mila      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ char    *ft_strncpy(char *dest, char *src, unsigned int n)
     return(dest);
 }
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int main()
-{
-    char dest[5];
-    //char dest1[5];
+// int main()
+// {
+//     char dest[5];
+//     //char dest1[5];
 
-    printf("mine: [%s]\n", ft_strncpy(dest, "Chaussures", 4));
-    //printf("Craft: [%s]\n", strncpy(dest1, "Chaussures", 4));
-}
+//     printf("mine: [%s]\n", ft_strncpy(dest, "Chaussures", 4));
+//     //printf("Craft: [%s]\n", strncpy(dest1, "Chaussures", 4));
+// }
