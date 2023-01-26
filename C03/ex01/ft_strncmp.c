2b6046b1433f3debbae4748d27afd8352d5601d9
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-01-26 14:28:43 +0100

    ex01

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: Julien Milants <julien.milants@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 19:49:09 by Julien Mila       #+#    #+#             */
-/*   Updated: 2023/01/25 20:17:44 by Julien Mila      ###   ########.fr       */
+/*   Updated: 2023/01/26 14:28:01 by Julien Mila      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,18 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
     unsigned int i;
 
     i = 0;
+    while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+    {   
+        if (s1[i] > s2[i])
+            return (1);
+        else if(s1[i] < s2[i])
+            return (-1);
+        i++;    
+    }
+    return (0);   
+}
+
+#include <stdio.h>
+int main(){
+    printf("%d", ft_strncmp("hello", "helloworld", 8));
 }
