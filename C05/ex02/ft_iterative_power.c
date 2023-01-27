28ba5a7641234d336e5647a19a94fcd95bfb7038
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-01-27 15:30:36 +0100

    c05 ex00 ex01 ex02 ex03 ex04

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,7 +6,18 @@
 /*   By: Julien Milants <julien.milants@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:36:49 by Julien Mila       #+#    #+#             */
-/*   Updated: 2023/01/27 13:36:53 by Julien Mila      ###   ########.fr       */
+/*   Updated: 2023/01/27 15:20:28 by Julien Mila      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_iterative_power(int nb, int power)
+{
+    int n;
+
+    if(power < 1 )
+        return(!power);
+    n = nb;
+    while (--power)
+        n *= nb;
+    return(n);
+}
