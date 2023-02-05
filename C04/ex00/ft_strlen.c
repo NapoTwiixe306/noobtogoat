5209a5ea2623c2a9876896aa6c9764e6805cbc85
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-05 17:39:36 +0100

    c04

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Julien Milants <julien.milants@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/05 16:57:39 by Julien Mila       #+#    #+#             */
-/*   Updated: 2023/02/05 17:02:09 by Julien Mila      ###   ########.fr       */
+/*   Created: 2023/02/05 17:00:58 by Julien Mila       #+#    #+#             */
+/*   Updated: 2023/02/05 17:03:42 by Julien Mila      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *str, char *to_find)
-{
-	int		i;
-	int		j;
-
-	i = 0;
-	if (to_find[0] == '\0')
-		return (str);
-	while (str[i] != '\0')
-	{
-		j = 0;
-		while (str[i + j] == to_find[j] && str[i + j] != '\0')
-		{
-			if (to_find[j + 1] == '\0')
-				return (&str[i]);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
