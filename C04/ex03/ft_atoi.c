29479933fe351b01482b920423102890d9c19f6e
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-07 10:45:53 +0100

    atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,51 @@
 /*   By: Julien Milants <julien.milants@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 23:04:28 by Julien Mila       #+#    #+#             */
-/*   Updated: 2023/02/05 23:10:53 by Julien Mila      ###   ########.fr       */
+/*   Updated: 2023/02/07 10:45:08 by Julien Mila      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int whiteSpace(char *str, int *ptr_i)
+{
+    int count;
+    int i;
+
+    i = 0;
+    count = 1;
+    while ((str[i] >= 9 && str[i] <=13) || str[i] == 32)
+        i++;
+    while(str[i] && (str[i] == 43 || str[i] == 45))
+    {
+        if(str[i] == 45)
+            count *= -1;
+        i++;
+    }
+    *ptr_i = i;
+    return(count);
+}
+
+int ft_atoi(char *str)
+{
+    int sign;
+    int result;
+    int i;
+
+    result = 0;
+    sign = whiteSpace(str, &i);
+    while (str[i] && str[i] >= 48 && str[i] <= 57)
+    {
+        result *= 10;
+        result += str[i] - 48;
+        i++;
+    }
+    result *= sign;
+    return (result);
+    
+}
+
+#include <stdio.h>
+int main(void)
+{
+	char *s = "   ---+--+01234506789ab567";
+	printf("%d", ft_atoi(s));
+}
